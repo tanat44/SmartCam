8ff02f11804227b9d1eb06e77f90ce9b6b22abd1
tanat44 <tanat44@gmail.com>
2020-04-28 15:06:44 +0700

    reflect big's comments, new ui design using sidebar

--- smartcam-frontend/src/App.js
+++ smartcam-frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import SearchPage from './SearchPage';
-import FixedMenuLayout from './FixedMenuLayout';
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div className="App">
       <SearchPage />
-      {/* <FixedMenuLayout/ > */}
     </div>
   );
 }
--- smartcam-frontend/src/SearchPage.js
+++ smartcam-frontend/src/SearchPage.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect,} from 'react';
-import { Segment, Header, Table, Label, Popup, Grid } from 'semantic-ui-react'
+import { Segment, Header, Table, Sidebar, Popup, Grid, Menu, Icon, Image, Label, Container } from 'semantic-ui-react'
 import { rawData } from './RawData';
 import { DateInput } from 'semantic-ui-calendar-react';
 import * as moment from 'moment';
-import './SearchPage.css';
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
 } from 'recharts';
 
 const SearchPage = () => {
 
   const COLOR_MAP = ["#E8E8F0", "#b3cde0", "#6497b1", "#005b96", "#03396c"]; //, "#011f4b"];
 
-  const [startDate, setStartDate] = useState(moment().format('DD-MM-YYYY'));
-  const [endDate, setEndDate] = useState(moment().format('DD-MM-YYYY'));
+  const [startDate, setStartDate] = useState(moment());
+  const [endDate, setEndDate] = useState(moment());
   const [originalBucket, setOriginalBucket] = useState([]);
   const [filteredBucket, setFilteredBucket] = useState([]);
   const [graphBucket, setGraphBucket] = useState([])
@@ -27,7 +26,7 @@ const SearchPage = () => {
   const [currentDay, setCurrentDay] = useState(0);
 
   useEffect (() => {
-    fetchData();
+    fetchData( startDate, endDate );
 
   }, [startDate, endDate])
 
@@ -72,7 +71,7 @@ const SearchPage = () => {
       data.name = `${2*i}-${2*i+2}`;
       for (let c=0; c < cameraNum; c++) {
         const count = filteredBucket[c][i][day].count;
-        data[`Camera${c}`] = count;
+        data[`Camera ${c}`] = count;
         if (count > maxCount){
           maxCount = count;
         }
@@ -143,7 +142,7 @@ const SearchPage = () => {
         if(d.count > 0)
           label = Array.from(d.labels).sort().join(', ');
         return (
-            <Popup trigger={
+            <Popup key={i} basic trigger={
                 <Table.Cell style={{padding:p, border:"0px"}}>
                   <div style={{width:"20px", height:"20px", backgroundColor:color}} onClick={() => calculateGraphBucket(i, cameraIndex)}/>
                 </Table.Cell>
@@ -157,7 +156,7 @@ const SearchPage = () => {
                   <div style={{marginBottom: '1rem'}}>
                     <Grid.Row textAlign='left'>
                       <p>
-                        <b>Date </b> {moment(startDate, 'DD-MM-YYYY').add(i, 'days').format('D/M/YYYY')}
+                        <b>Date </b> {moment(startDate).add(i, 'days').format('D/M/YYYY')}
                       </p>
                     </Grid.Row>
                     <Grid.Row textAlign='left'>
@@ -182,28 +181,32 @@ const SearchPage = () => {
     const manyRows = data.map((d, i) => {
       const hourStart = 2*i;
       const hourEnd = 2*i+2;
-      return (<Table.Row key={i}>
-        <Table.Cell style={{ padding: p, border:"0px", whiteSpace: 'nowrap', fontWeight: 'bold', padding: '0 0.5rem', borderTop: i === 6 ? '1px solid gray' : '0px', textAlign: 'center' }}>
-          {/* {hourStart}-{hourEnd} */}
-          { i === 0 ? 'AM' : ''}
-          { i === 6 ? 'PM' : ''}
-        </Table.Cell>
-        {cameraRow(d, hourStart, hourEnd)}
-      </Table.Row>);
+      return (
+        <Table.Row key={i}>
+          <Table.Cell style={{ padding: p, border:"0px", whiteSpace: 'nowrap', fontWeight: 'bold', padding: '0 0.5rem 0 0', borderTop: i === 6 ? '1px solid gray' : '0px', textAlign: 'center' }}>
+            { i === 0 ? 'AM' : ''}
+            { i === 6 ? 'PM' : ''}
+          </Table.Cell>
+          {cameraRow(d, hourStart, hourEnd)}
+        </Table.Row>
+      );
     });
 
     const headers = Array.apply(null, Array(dayNum)).map((v , i) => {
-      return <Table.HeaderCell style={{ padding:p, border:"0px", whiteSpace: 'nowrap', textOverflow: 'ellipsis', maxWidth: '20px'}}>   
-        { i%7 == 0 ? moment(startDate, 'DD-MM-YYYY').add(i, 'days').format('D/M') : ''}
+      return <Table.HeaderCell style={{ padding:p, border:"0px", whiteSpace: 'nowrap', textOverflow: 'ellipsis', maxWidth: '20px'}} key={i}>   
+        { i%7 === 0 ? moment(startDate).add(i, 'days').format('D/M') : ''}
         </Table.HeaderCell>;
     });
 
     return (
-        <Table collapsing basic size='small' style={{padding:0, border:"0px"}}>
+        <Table style={{padding:0, border:"0px"}}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell style={{padding:p, border:"0px"}} />
+              {headers}
+            </Table.Row>
+          </Table.Header>
           <Table.Body>
-            <Table.HeaderCell style={{padding:p, border:"0px"}}>
-            </Table.HeaderCell>
-            {headers}
             {manyRows}
           </Table.Body>
         </Table>
@@ -211,95 +214,145 @@ const SearchPage = () => {
   };
 
   function handleDatePicker (e, {name, value}) {
+
+    const date = moment(value, 'DD-MM-YYYY');
     if (name === 'endDate') {
-      setEndDate(moment(value).format('DD-MM-YYYY'));
-      if (moment(value) < moment(startDate)) {
-        setStartDate(moment(value).format('DD-MM-YYYY'));
+      setEndDate(date);
+      if (date.isBefore(startDate)) {
+        setStartDate(date);
       }
+      console.log('End date', value);
     } else if (name === 'startDate') {
-      setStartDate(moment(value).format('DD-MM-YYYY'));
-      if (moment(value) > moment(endDate)) {
-        setEndDate(moment(value).format('DD-MM-YYYY'));
+      setStartDate(date);
+      if (date.isAfter(endDate)) {
+        setEndDate(date);
       }
+      console.log('Start date', value);
     }
   }
 
+
   return (
-    <div className="screen">
-      <Header as='h1' content ='Search Playback' textAlign='left' />
-      <div style={{margin: '2rem 0'}}>
-        From 
-        <div style={{display: "inline-block", margin: "0 10px"}}>
-          <DateInput
-            name="startDate"
-            placeholder="From"
-            value={startDate}
-            iconPosition="left"
-            onChange={handleDatePicker}
-          />
-        </div>
-        To 
-        <div style={{display: "inline-block", margin: "0 10px"}}>
-          <DateInput
-            name="endDate"
-            placeholder="To"
-            value={endDate}
-            iconPosition="left"
-            onChange={handleDatePicker}
-          />
-        </div>
-        Search
-        <div className="ui input" style={{display: "inline-block", margin: "0 10px"}}> <input type="text" placeholder="Keyword..." value={keyword} onChange={handleSearch}/></div>
-      </div>
-      <div style={{display: 'flex', flexFlow: 'row', flex: '1 1 auto', overflow: 'auto'}}> 
-        <div style={{display: 'flex', flex: '0 1 auto', overflow: 'auto', flexDirection: 'column', maxWidth: showGraph ? '70%' : '100%'}} >
+    <Sidebar.Pushable>
+      <Sidebar
+        as={Menu}
+        animation='overlay'
+        icon='labeled'
+        vertical
+        visible={!showGraph}
+      >
+        <Grid textAlign='center' style={{padding: '2rem'}}>
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <Header as='h1' textAlign='left' >Search Playback</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row textAlign='left'>
+            <div style={{margin: '0 0 0 1rem'}}>
+              From 
+              <div style={{display: "inline-block", margin: "0 10px"}}>
+                <DateInput
+                  name="startDate"
+                  placeholder="From"
+                  value={startDate.format('DD-MM-YYYY')}
+                  iconPosition="left"
+                  onChange={handleDatePicker}
+                />
+              </div>
+              To 
+              <div style={{display: "inline-block", margin: "0 10px"}}>
+                <DateInput
+                  name="endDate"
+                  placeholder="To"
+                  value={endDate.format('DD-MM-YYYY')}
+                  iconPosition="left"
+                  onChange={handleDatePicker}
+                />
+              </div>
+              Search
+              <div className="ui input" style={{display: "inline-block", margin: "0 10px"}}> <input type="text" placeholder="Keyword..." value={keyword} onChange={handleSearch}/></div>
+            </div>
+          </Grid.Row>
           {
             filteredBucket.map((b, i) => {
               return (
-                <div style={{display: 'flex'}}>
-                  <Segment key={i}>
-                    <Label color='blue' size='big'>Camera {i+1}</Label>
+                <Grid.Row key={i}>
+                  <Segment padded>
+                    <Label color='blue' attached='top' size='large'>Camera {i}</Label>
                     {cameraTable(b, i)}
                   </Segment>
-                </div>
+                </Grid.Row>
               );
             })
           }
-        </div>
+        </Grid>
+      </Sidebar>
+
+      <Sidebar.Pusher>
         { showGraph && 
-          <div style={{display: 'flex', flex: '1 1 auto', flexDirection: 'column'}}>
-            <div style={{display: 'flex', marginLeft: '2rem', fontWeight: 'bold'}}>
-              Detections from Camera {currentCamera}
-            </div>
-            <div style={{display: 'flex', margin: '0 0 1rem 2rem'}}>
-              Date {moment(startDate, 'DD-MM-YYYY').add(currentDay, 'days').format('D/M/YYYY')}
-            </div>
-            <LineChart
-              width={500}
-              height={300}
-              data={graphBucket}
-              margin={{
-                top: 5, right: 30, left: 20, bottom: 5,
-              }}
-              style={{display: 'flex'}}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" interval={0}/>
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              {
-                filteredBucket.map((c, index) => {
-                  const label = `Camera${index + 1}`;
-                  return index === currentCamera ? 
-                    <Line type="monotone" dataKey={label} stroke="#0e659d" strokeWidth={3} activeDot={{ r: 8 }} /> : 
-                    <Line type="monotone" dataKey={label} stroke="#bbb" />
-                })
-              }
-            </LineChart>
-        </div>}
-      </div>     
-    </div>
+          <Segment basic>
+            <Grid container columns={2}>
+              <Grid.Column>
+                <Header as='h3'>Detections from Camera {currentCamera} </Header>
+                  Date {moment(startDate).add(currentDay, 'days').format('D/M/YYYY')}
+              </Grid.Column>
+              <Grid.Column textAlign='right'>
+                <button className="ui secondary button" onClick={() => setShowGraph(false)}>Close</button>
+              </Grid.Column>
+            </Grid>
+            <div style={{width: '100%', height: '300px'}}>
+              <ResponsiveContainer>
+                <LineChart
+                    data={graphBucket}
+                    margin={{
+                      top: 40, right: 80, left: 80, bottom: 40,
+                    }}
+                    
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" interval={0} angle={-45} textAnchor="end"/>
+                    <YAxis />
+                    <Tooltip />
+                    {
+                      filteredBucket.map((c, index) => {
+                        const label = `Camera ${index}`;
+                        return index === currentCamera ? 
+                          <Line type="monotone" dataKey={label} stroke="#0e659d" strokeWidth={3} activeDot={{ r: 8 }} key={index} /> : 
+                          <Line type="monotone" dataKey={label} stroke="#bbb" key={index} />
+                      })
+                    }
+                  </LineChart>
+                </ResponsiveContainer>
+              </div>
+              <Container>
+                <Header as='h3'>Detail Information</Header>
+                <Table basic='very' celled collapsing style={{marginLeft: '3rem'}}>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.HeaderCell>Time (hour)</Table.HeaderCell>
+                      <Table.HeaderCell>Detected Label</Table.HeaderCell>
+                    </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
+                    {
+                      filteredBucket[currentCamera].map( (hour, i) => {
+                        const label = Array.from(hour[currentDay].labels).sort().join(', ');
+                        return (
+                          <Table.Row key={i}>
+                            <Table.Cell textAlign='center'>{2*i}-{2*i+2}</Table.Cell>
+                            <Table.Cell>{label}</Table.Cell>
+                          </Table.Row>
+                        );
+                      })
+                    }
+                  </Table.Body>
+                </Table>
+              </Container>
+              
+          </Segment>
+        }
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
   );
 
 };
